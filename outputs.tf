9892f899da8cc6a159b8f10d44d0c03232ff378b
Suraj <ecesuraj2013@gmail.com>
2026-08-10 17:40:54 +0530

    decomissioning

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-output "bucket_name" {
-  value = aws_s3_bucket.example.bucket
-}
+# output "bucket_name" {
+#   value = aws_s3_bucket.example.bucket
+# }
 
-output "bucket_arn" {
-  value = aws_s3_bucket.example.arn
-}
+# output "bucket_arn" {
+#   value = aws_s3_bucket.example.arn
+# }
