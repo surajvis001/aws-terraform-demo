2592ce4efce6063b5afb4ad0a73fc09554546404
surajvis001 <41757584+surajvis001@users.noreply.github.com>
2026-08-10 14:39:28 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -53,11 +53,3 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
     }
   }
 }
-
-output "bucket_name" {
-  value = aws_s3_bucket.example.bucket
-}
-
-output "bucket_arn" {
-  value = aws_s3_bucket.example.arn
-}
