8c72f39154c3a3aa34475428ec4e5b001075074d
Suraj <ecesuraj2013@gmail.com>
2026-08-10 17:39:30 +0530

    decomissioning

--- main.tf
+++ main.tf
@@ -1,40 +1,40 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "cloud-crafter-s3-bucket"
+# resource "aws_s3_bucket" "example" {
+#   bucket = "cloud-crafter-s3-bucket"
 
-  tags = {
-    Name        = "My S3 Bucket"
-    Environment = "dev"
-  }
-}
+#   tags = {
+#     Name        = "My S3 Bucket"
+#     Environment = "dev"
+#   }
+# }
 
-# Enable versioning
-resource "aws_s3_bucket_versioning" "example" {
-  bucket = aws_s3_bucket.example.id
+# # Enable versioning
+# resource "aws_s3_bucket_versioning" "example" {
+#   bucket = aws_s3_bucket.example.id
 
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
 
-######3
+# ######3
 
-# Block all public access
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.example.id
+# # Block all public access
+# resource "aws_s3_bucket_public_access_block" "example" {
+#   bucket = aws_s3_bucket.example.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
 
-# Enable server-side encryption
-resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
-  bucket = aws_s3_bucket.example.id
+# # Enable server-side encryption
+# resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+#   bucket = aws_s3_bucket.example.id
 
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+# }
